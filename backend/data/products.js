12f7056c7b8e6829f0dfa8e56c126b56cfa90096
Tashen Dinal <10820865@students.plymouth.ac.uk>
2024-01-06 19:02:14 +0530

    First Commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: '1',
+    image: '/images/1.jpg',
+    description:
+      ' 1',
+    brand: '1',
+    category: 'Electronics',
+    price: 20.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'i2',
+    image: '/images/2.jpg',
+    description:
+      ' 2',
+    brand: '2',
+    category: 'Electronics',
+    price: 20.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: '3',
+    image: '/images/camera.jpg',
+    description:
+      '03',
+    brand: '03',
+    category: 'Electronics',
+    price: 30.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: '04',
+    image: '/images/04.jpg',
+    description:
+      '04',
+    brand: '04',
+    category: 'Electronics',
+    price: 20.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: '05',
+    image: '/images/05.jpg',
+    description:
+      '05',
+    brand: '05',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: '06',
+    image: '/images/06.jpg',
+    description:
+      '06',
+    brand: '06',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
