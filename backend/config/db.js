a182c2f8bcba9ff03390d913dda220563c463311
tashendinal <10820865@students.plymouth.ac.uk>
2024-04-24 22:57:41 +0500

    Deployment Issue Resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/store", {
+    const conn = await mongoose.connect("mongodb+srv://deployment_user:iZDUyoZiBXr7Omhx@store.zu8iqkv.mongodb.net", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
