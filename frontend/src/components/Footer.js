264f0101cf3f5139f9330663553abad7243a3497
Tashen Dinal <10820865@students.plymouth.ac.uk>
2024-04-24 21:00:11 +0530

    update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; EchoMart - Group Project</Col> 
+          <Col className='text-center py-3'>Copyright &copy; EcoChic - Group Project</Col> 
         </Row>
       </Container>
     </footer>
